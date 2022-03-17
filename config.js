54ed6c81b8eca46f43bbb764a1a7e6de0c255ec9
459900 <shadowboy254.rblx@gmail.com>
2022-03-17 14:35:09 +0530

    color changes

--- config.js
+++ config.js
@@ -2,6 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+      apiKey: "AIzaSyCPGq-bcTU5EeXIIwe5b5_FElws7hB7aBM",
+      authDomain: "vote-2e69c.firebaseapp.com",
+      projectId: "vote-2e69c",
+      storageBucket: "vote-2e69c.appspot.com",
+      messagingSenderId: "737991868518",
+      appId: "1:737991868518:web:48ff2b4e990ce0c7deada7"
+    
+    
 
 };
 // Initialize Firebase
